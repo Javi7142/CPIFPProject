a413795b6a4a6650d81a6484a5ca78798107e873
Javier Hons Barroso <jhonbar1407@cpifpalanturing.es>
2026-03-13 20:28:56 +0100

    first commit

---
+++ src/Profesores.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+import com.modeliosoft.modelio.javadesigner.annotations.objid;
+
+@objid ("cc36f097-3e20-44ea-aacf-5328db528b41")
+public class Profesores {
+    @objid ("06bc4192-6120-45b2-90b8-bc4c17d25313")
+    private String nombre;
+
+    @objid ("03b11f73-a3bc-452d-bab6-53e2db27b7c7")
+    private String especialidad;
+
+    @objid ("7f733bc8-f15c-40d9-a6c5-9cca71bf79ae")
+    private List<Modulo> listaModulos = new ArrayList<Modulo> ();
+
+    @objid ("7d695cb3-43fd-4371-9db9-9ad6d6fd9663")
+    private Departamentos listaDepartamentos;
+
+    @objid ("2c2f473c-7bf9-4e33-95fc-91a737b162cc")
+    private Departamentos esJefe;
+
+    @objid ("eb59ba8b-d6c3-4b5a-bc0e-7505360861bf")
+    public Departamentos getEsJefe() {
+        // Automatically generated method. Please delete this comment before entering specific code.
+        return this.esJefe;
+    }
+
+    @objid ("498b2963-7efd-4eb0-ab59-2c885e9636c6")
+    public void setEsJefe(final Departamentos value) {
+        // Automatically generated method. Please delete this comment before entering specific code.
+        this.esJefe = value;
+    }
+
+}
